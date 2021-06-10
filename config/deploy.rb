d1dad98efd3e81c967ba51a97388ef62cfded939
Obfuscoder <obfuscoder@obfusco.de>
2021-06-10 23:16:22 +0200

    Add a few asset configs to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,10 @@ lock "~> 3.16.0"
 set :application, "fpt"
 set :repo_url, "https://github.com/obfuscoder/fpt.git"
 
+set :assets_manifests, ['app/assets/config/manifest.js']
+set :rails_assets_groups, :assets
+set :normalize_asset_timestamps, %w{public/images public/javascripts public/stylesheets}
+
 set :deploy_to, '/home/fpt/www2'
 
 append :linked_dirs, 'log', 'tmp'
